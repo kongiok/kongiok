98d0a21f85414efd35bc0ad2f90b4628c9cdf864
Marx Wang <211410146@o365.tku.edu.tw>
2024-08-28 20:23:09 +0800

    chore(test): set env config.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,13 @@ import { defineVitestConfig } from "@nuxt/test-utils/config";
 export default defineVitestConfig({
   test: {
     environment: "nuxt",
+    environmentOptions: {
+      nuxt: {
+        mock: {
+          intersectionObserver: true,
+          indexedDB: true,
+        },
+      },
+    },
   },
 });
