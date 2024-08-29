4cafd3e9d74ce692cd9772373ec07ec4c3e9ae5c
Marx Wang <211410146@o365.tku.edu.tw>
2024-08-29 14:05:57 +0800

    feat(ui): add nuxt ui framework

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/test-utils/module"],
+  modules: ["@nuxt/eslint", "@nuxt/test-utils/module", "@nuxt/ui"],
   eslint: {
     config: {
       stylistic: true,
