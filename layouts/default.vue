fb1473468a4d9bc56e3ba71e21a4e326cda3ce23
Marx Wang <211410146@o365.tku.edu.tw>
2024-09-03 22:08:27 +0800

    feat(pages): separate single page application into multi page

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.wrapper {
+  @apply h-dvh w-dvw;
+  @apply flex flex-col items-center justify-center;
+  @apply bg-concrete-200;
+}
+</style>
